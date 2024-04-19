9f464b3536dbe46e7ed7d35265f652dc3cc50f9d
sobiahmed1988 <160466665+sobiahmed1988@users.noreply.github.com>
2024-04-19 13:10:08 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 10000; // Dollar
+
+let myPin = 1234;
+
+let pinAnswer = await inquirer.prompt({
+  name: "pin",
+  message: "Enter your pin",
+  type: "number",
+});
+
+if (pinAnswer.pin === myPin) {
+  console.log("Your Pin is Correct");
+
+  let operationAns = await inquirer.prompt([
+    {
+      name: "operation",
+      message: "please select option",
+      type: "list",
+      choices: ["withdraw", "Fast cash", "check balance"],
+    },
+  ]);
+
+  if (operationAns.operation === "withdraw") {
+    let amountAns = await inquirer.prompt([
+      {
+        name: "amount",
+        message: "Enter your amount",
+        type: "number",
+      },
+    ]);
+    if (amountAns.amount <= myBalance) {
+      let balance = myBalance - amountAns.amount;
+      console.log(`The remaining balance is ${balance}`);
+    } else {
+      console.log("You have Insufficient balance");
+    }
+  } else if (operationAns.operation === "Fast cash") {
+    let FastcashAns = await inquirer.prompt([
+      {
+        name: "amount",
+        type: "list",
+        choices: ["1000", "3000", "5000", "15000"],
+      },
+    ]);
+    if (FastcashAns.amount <= myBalance) {
+      let balance2 = myBalance - FastcashAns.amount;
+      console.log(`The remaining balance is ${balance2}`);
+    } else {
+      console.log("You have Insufficient balance");
+    }
+  } else if (operationAns.operation === "check balance") {
+    console.log(myBalance);
+  }
+} else {
+  console.log("Your pin is wrong");
+}
